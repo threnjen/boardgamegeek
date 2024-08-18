f5a5312e43d581fe4351ad9914364d56fbe2e47c
threnjen <threnjen@gmail.com>
2024-08-18 10:26:44 -0700

    directory structure changes

--- game_data_scraper/config.py
+++
@@ -1,4 +0,0 @@
-ENV = os.environ.get("ENV", "dev")
-S3_BUCKET= os.environ.get("S3_SCRAPER_BUCKET")
-URLS_PREFIX= os.environ.get("JSON_URLS_PREFIX")
-SCRAPER_TASK_DEFINITION= os.environ.get("SCRAPER_TASK_DEFINITION")
