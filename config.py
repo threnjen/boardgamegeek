3c4458fdbef27b9aa5660d06f3a6f94bd8a53b45
threnjen <threnjen@gmail.com>
2024-08-18 10:25:33 -0700

    add config to main area

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+ENV = os.environ.get("ENV", "dev")
+S3_SCRAPER_BUCKET= os.environ.get("S3_SCRAPER_BUCKET")
+JSON_URLS_PREFIX= os.environ.get("JSON_URLS_PREFIX")
+SCRAPER_TASK_DEFINITION= os.environ.get("SCRAPER_TASK_DEFINITION")
