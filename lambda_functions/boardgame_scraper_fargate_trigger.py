0079bce2d74e69da21670e940c258aae99db0b31
threnjen <threnjen@gmail.com>
2024-08-18 10:26:08 -0700

    updates/fixes to lambda functions functionality

--- lambda_functions/boardgame_scraper_fargate_trigger.py
+++ lambda_functions/boardgame_scraper_fargate_trigger.py
@@ -8,7 +8,8 @@ SCRAPER_TASK_DEFINITION= os.environ.get("SCRAPER_TASK_DEFINITION")
 def get_filenames():
 
     if ENV != "prod":
-        file_prefixes = os.listdir("data_store/local_files/scraper_urls_raw")
+        raw_files = os.listdir("data_store/local_files/scraper_urls_raw")
+        file_prefixes = [x for x in raw_files if x.endswith(".json")]
     else:
         s3_client = boto3.client("s3")
         raw_files = s3_client.list_objects_v2(Bucket=S3_BUCKET, Prefix=URLS_PREFIX)["Contents"]
@@ -25,22 +26,23 @@ def lambda_handler(event, context):
 
     ecs_client = boto3.client("ecs", region_name="us-west-2")
     
-    task_arn = (
-        ecs_client.describe_task_definition(taskDefinition=task_definition)
-        .get("taskDefinition")
-        .get("taskDefinitionArn")
-    )
-    print(task_arn)
+    # task_arn = (
+    #     ecs_client.describe_task_definition(taskDefinition=task_definition)
+    #     .get("taskDefinition")
+    #     .get("taskDefinitionArn")
+    # )
+    # print(task_arn)
 
     latest_version = (
-    ecs.describe_task_definition(taskDefinition=task_definition)
+    ecs_client.describe_task_definition(taskDefinition=task_definition)
     .get("taskDefinition")
     .get("revision")
 )
 
     for file in file_prefixes:
-        filename = f"s3://{S3_BUCKET}/{file}"
+        filename = file.split("/")[-1]
         print(filename)
+        continue
 
         response = ecs_client.run_task(
             taskDefinition=f"{task_definition}:{latest_version}",
