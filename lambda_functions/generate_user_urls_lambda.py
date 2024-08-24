392cb234f3635f8f888cf6cd9dbdcf20b9db55b1
threnjen <threnjen@gmail.com>
2024-08-24 09:33:05 -0700

    working game user scraper lambda

--- lambda_functions/generate_user_urls_lambda.py
+++ lambda_functions/generate_user_urls_lambda.py
@@ -2,28 +2,32 @@ import pandas as pd
 import json
 import os
 import awswrangler as wr
-import math
-import boto3
+import numpy as np
 
 ENV = os.environ.get("ENV", "dev")
 S3_SCRAPER_BUCKET = os.environ.get("S3_SCRAPER_BUCKET")
-JSON_URLS_PREFIX = os.environ.get("JSON_URLS_PREFIX")
 url_block_size = 20
 number_url_files = 30
+NUMBER_PROCESSES = 50
 
 
-def generate_raw_urls(game_ids: list[str]):
-    """Generate the raw urls for the scraper"""
-    targets = [
-        game_ids[i : i + url_block_size]
-        for i in range(0, len(game_ids), url_block_size)
-    ]
-    print(f"Generated {len(targets)} URLS with block size {url_block_size}")
+def generate_ratings_urls(game_entries):
+    urls_list = []
+    bgg_ids = game_entries[0]
+    ratings_pages = game_entries[1]
 
-    return [
-        f"https://www.boardgamegeek.com/xmlapi2/thing?id={','.join(block)}&stats=1&type=boardgame"
-        for block in targets
-    ]
+    for i in list(range(0, len(bgg_ids))):
+        current_bgg_id = bgg_ids[i]
+        max_page_number = ratings_pages[i]
+        page_numbers = range(1, max_page_number + 1)
+        print(f"{current_bgg_id} - {max_page_number} pages")
+
+        for page_number in page_numbers:
+            path = f"https://www.boardgamegeek.com/xmlapi2/thing?id={current_bgg_id}&ratingcomments=1&page={str(page_number)}&pagesize=100"
+            urls_list.append(path)
+    print("\n")
+
+    return urls_list
 
 
 def lambda_handler(event, context):
@@ -31,7 +35,7 @@ def lambda_handler(event, context):
     # Get this file manually from https://boardgamegeek.com/data_dumps/bg_ranks
     if ENV == "dev":
         print("Reading the file locally")
-        df = pd.read_pickle("game_data_cleaner/game_dfs_dirty/games.pkl")
+        games = pd.read_pickle("../game_data_cleaner/game_dfs_dirty/games.pkl")
     else:
         print("Reading the file from S3")
         # download the pickle file from S3
@@ -39,40 +43,55 @@ def lambda_handler(event, context):
             path=f"s3://{S3_SCRAPER_BUCKET}/game_dfs_dirty/games.pkl",
             local_file="games.pkl",
         )
-        df = pd.read_pickle("games.pkl")
+        games = pd.read_pickle("games.pkl")
 
-    return
+    ratings_totals = pd.DataFrame(games["BGGId"])
+    ratings_totals["RatingsPages"] = np.ceil(games["NumUserRatings"].astype(int) / 100).astype("int")
+    ratings_totals = ratings_totals.sort_values(
+        "RatingsPages", ascending=False
+    ).reset_index(drop=True)
 
-    game_ids = df["id"].astype(str).to_list()
-    print(f"Number of game ids: {len(game_ids)}")
+    total_ratings = ratings_totals["RatingsPages"].sum()
+    max_ratings_pages = int(np.ceil(total_ratings/NUMBER_PROCESSES))
+    max_ratings_pages
 
-    scraper_urls_raw = generate_raw_urls(game_ids)
+    df_groups = {}
+    group_counter = 1
+    position = 0
 
-    print(len(scraper_urls_raw))
-    url_block_size = math.ceil(len(scraper_urls_raw) / number_url_files)
-    print(f"URL block size: {url_block_size}")
+    while len(ratings_totals) > 0:
 
-    # divide the list of scraper_raw_urls into number_url_files parts
-    # and save them in separate files
+        chunk_size = 0
+        bgg_id_lists = []
+        ratings_lists = []
 
-    local_path = "data_store/local_files/scraper_urls_raw" if ENV != "prod" else "/tmp"
+        while max_ratings_pages > chunk_size:
+            try:
+                num_ratings = ratings_totals.iloc[position]['RatingsPages']
+                bgg_id = ratings_totals.iloc[position]['BGGId']
+                bgg_id_lists.append(bgg_id)
+                ratings_lists.append(num_ratings)
+                chunk_size += num_ratings
+                ratings_totals = ratings_totals.drop(position)
+                position += 1
 
-    for i in range(number_url_files):
-        print(f"Saving block size {i * url_block_size} : {(i + 1) * url_block_size}")
-        with open(f"{local_path}/scraper_urls_raw_{i}.json", "w") as convert_file:
-            convert_file.write(
-                json.dumps(
-                    scraper_urls_raw[i * url_block_size : (i + 1) * url_block_size]
-                )
-            )
-        if ENV == "prod":
-            # upload the json to S3 with boto3
-            s3_client = boto3.client("s3")
-            s3_client.upload_file(
-                f"{local_path}/scraper_urls_raw_{i}.json",
-                S3_SCRAPER_BUCKET,
-                f"{JSON_URLS_PREFIX}/scraper_urls_raw_{i}.json",
-            )
+            except:
+                break
+
+        # print(chunk_size)
+        if len(bgg_id_lists) == 0:
+            break
+        df_groups[f"group{group_counter}"] = [bgg_id_lists, ratings_lists]
+        # print(f"group{group_counter} Complete - {len(bgg_id_lists)} games with {chunk_size} ratings")
+        group_counter += 1
+    
+    group_urls = {}
+
+    for group, game_entries in df_groups.items():
+        group_urls = generate_ratings_urls(game_entries)
+
+        with open(f"../game_user_scraper/scraper_urls_raw/{group}_scraper_urls_ratings.json", "w") as convert_file:
+            convert_file.write(json.dumps(group_urls))
 
 
 if __name__ == "__main__":
