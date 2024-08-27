24da1d15445e10a590ee11a039e062cd56f8a4c7
threnjen <threnjen@gmail.com>
2024-08-26 19:40:18 -0700

    cleanup

--- bgg_boardgame_file_retrieval/get_bgg_games_file.py
+++ bgg_boardgame_file_retrieval/get_bgg_games_file.py
@@ -91,17 +91,12 @@ def lambda_handler(event, context):
 
     time.sleep(10)
 
-    print(os.listdir(f"{DEFAULT_DIRECTORY}/Downloads"))
-
     # if directory DEFAULT_DIRECTORY/Downloads does not exist, create it
     if not os.path.exists(f"{DEFAULT_DIRECTORY}/Downloads"):
         os.makedirs(f"{DEFAULT_DIRECTORY}/Downloads")
 
     with zipfile.ZipFile(f"{DEFAULT_DIRECTORY}/Downloads/{filename}", "r") as zip_ref:
         zip_ref.extractall(DEFAULT_DIRECTORY)
-
-    print(os.listdir(f"{DEFAULT_DIRECTORY}/Downloads"))
-    print(os.listdir(f"{DEFAULT_DIRECTORY}"))
 
     wr.s3.upload(
         local_file=f"{DEFAULT_DIRECTORY}/boardgames_ranks.csv",
