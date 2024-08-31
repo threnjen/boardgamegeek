1010afaed73da0f2c686f83d803c1e5e764e44ad
aherrmann85 <74567621+aherrmann85@users.noreply.github.com>
2024-08-31 11:00:14 -0700

    only save config if running on local

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ import os
 from utils.s3_file_handler import S3FileHandler
 from utils.local_file_handler import LocalFileHandler
 
+IS_LOCAL = True if os.environ.get("IS_LOCAL", "False") == "True" else False
+
 CONFIGS = S3FileHandler().load_file(file_path="config.json")["CONFIGS"]
-LocalFileHandler().save_file(file_path="config.json", data=CONFIGS)
+if IS_LOCAL:
+    LocalFileHandler().save_file(file_path="config.json", data=CONFIGS)
