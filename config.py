08380c64024b01aabf2f9983f42fbc74c8c1ba6c
threnjen <threnjen@gmail.com>
2024-09-01 10:50:32 -0700

    add some descriptive print statements to config

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ from datetime import datetime
 
 IS_LOCAL = True if os.environ.get("IS_LOCAL", "False") == "True" else False
 
+print(f"\nChecking for local config file and evaluating for updates from S3.")
+
 if LocalFileHandler().check_file_exists(file_path="config.json"):
     local_datetime = LocalFileHandler().get_last_modified(file_path="config.json")
     NOT_FOUND = False
@@ -19,8 +21,11 @@ if (
     NOT_FOUND
     or S3FileHandler().get_last_modified(file_path="config.json") > local_datetime
 ):
+    print("Loading config from S3")
     CONFIGS = S3FileHandler().load_file(file_path="config.json")
     if IS_LOCAL:
+        print("Saving config to local")
         LocalFileHandler().save_file(file_path="config.json", data=CONFIGS)
 else:
+    print("Loading config from local")
     CONFIGS = LocalFileHandler().load_file(file_path="config.json")
