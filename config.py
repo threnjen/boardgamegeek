5c5c5e160ff37dd0a7223f8e93e40bd5c2c46cd1
threnjen <threnjen@gmail.com>
2024-09-01 10:31:25 -0700

    update typo in local load

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ s3_configs = S3FileHandler().load_file(file_path="config.json")["CONFIGS"]
 
 # Try to load configurations locally, if available
 try:
-    local_configs = LocalFileHandler().load_file(file_path="config.json")["CONFIGS"]
+    local_configs = LocalFileHandler().load_file(file_path="config.json")
 except FileNotFoundError:
     local_configs = None
 
--- push_local_config_to_s3.py
+++ push_local_config_to_s3.py
@@ -1,5 +1,7 @@
 from utils.s3_file_handler import S3FileHandler
 from utils.local_file_handler import LocalFileHandler
+import json
 
-CONFIGS = LocalFileHandler().load_file(file_path="config.json")["CONFIGS"]
-S3FileHandler().save_json(file_path="config.json", data=CONFIGS)
+CONFIGS = LocalFileHandler().load_file(file_path="config.json")
+print(CONFIGS)
+S3FileHandler().save_json(file_path="config.json", data=json.dumps(CONFIGS))
