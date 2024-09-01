cc4cebc074b8cf75558a5552c42967eed2ccab6a
threnjen <threnjen@gmail.com>
2024-09-01 10:46:40 -0700

    only pulls config from S3 if newer than local file

--- config.py
+++ config.py
@@ -1,23 +1,27 @@
 import boto3
 import os
+import pytz
 from utils.s3_file_handler import S3FileHandler
 from utils.local_file_handler import LocalFileHandler
+from datetime import datetime
 
 IS_LOCAL = True if os.environ.get("IS_LOCAL", "False") == "True" else False
 
-# Load configurations from S3
-s3_configs = S3FileHandler().load_file(file_path="config.json")["CONFIGS"]
+if LocalFileHandler().check_file_exists(file_path="config.json"):
+    local_datetime = LocalFileHandler().get_last_modified(file_path="config.json")
+    local_datetime = local_datetime.astimezone(pytz.utc)
+    NOT_FOUND = False
+else:
+    print("Local config not found")
+    local_datetime = datetime(1970, 1, 1).astimezone(pytz.utc)
+    NOT_FOUND = True
 
-# Try to load configurations locally, if available
-try:
-    local_configs = LocalFileHandler().load_file(file_path="config.json")
-except FileNotFoundError:
-    local_configs = None
-
-# Determine the final CONFIGS based on comparison between S3 and local configurations
-if s3_configs != local_configs:
-    CONFIGS = s3_configs
+if (
+    NOT_FOUND
+    or S3FileHandler().get_last_modified(file_path="config.json") > local_datetime
+):
+    CONFIGS = S3FileHandler().load_file(file_path="config.json")
     if IS_LOCAL:
         LocalFileHandler().save_file(file_path="config.json", data=CONFIGS)
 else:
-    CONFIGS = local_configs
+    CONFIGS = LocalFileHandler().load_file(file_path="config.json")
