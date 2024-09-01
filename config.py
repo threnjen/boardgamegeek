63970c5faad2ba941d6135bedee3b4891cf9f93f
threnjen <threnjen@gmail.com>
2024-09-01 10:26:42 -0700

    only use S3 config if it is updated vs local config

--- config.py
+++ config.py
@@ -5,6 +5,19 @@ from utils.local_file_handler import LocalFileHandler
 
 IS_LOCAL = True if os.environ.get("IS_LOCAL", "False") == "True" else False
 
-CONFIGS = S3FileHandler().load_file(file_path="config.json")["CONFIGS"]
-if IS_LOCAL:
-    LocalFileHandler().save_file(file_path="config.json", data=CONFIGS)
+# Load configurations from S3
+s3_configs = S3FileHandler().load_file(file_path="config.json")["CONFIGS"]
+
+# Try to load configurations locally, if available
+try:
+    local_configs = LocalFileHandler().load_file(file_path="config.json")["CONFIGS"]
+except FileNotFoundError:
+    local_configs = None
+
+# Determine the final CONFIGS based on comparison between S3 and local configurations
+if s3_configs != local_configs:
+    CONFIGS = s3_configs
+    if IS_LOCAL:
+        LocalFileHandler().save_file(file_path="config.json", data=CONFIGS)
+else:
+    CONFIGS = local_configs
