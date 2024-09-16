2a292cd1e5e6c19577cdbc4224ebd4efabff5e9e
threnjen <threnjen@gmail.com>
2024-09-16 15:02:07 -0700

    Add dagster orchestration to monorepo

---
+++ bgg_orchestrator/__init__.py
@@ -0,0 +1,32 @@
+from dagster import Definitions, load_assets_from_modules
+import os
+
+from .assets import assets
+from .resources import (
+    S3Resource,
+    DynamoDBResource,
+    LambdaHandlerResource,
+    ECSResource,
+    ConfigResource,
+)
+
+scraper_assets = load_assets_from_modules([assets])
+
+all_jobs = []
+
+defs = Definitions(
+    assets=[*scraper_assets],
+    resources={
+        "s3_resource": S3Resource(
+            region_name="us-west-2",
+        ),
+        "dynamodb_resource": DynamoDBResource(
+            region_name="us-west-2", table_name="boardgamegeek"
+        ),
+        "lambda_resource": LambdaHandlerResource(region_name="us-west-2"),
+        "ecs_resource": ECSResource(region_name="us-west-2"),
+        "config_resource": ConfigResource(
+            region_name="us-west-2", bucket=os.environ.get("S3_SCRAPER_BUCKET")
+        ),
+    },
+)
