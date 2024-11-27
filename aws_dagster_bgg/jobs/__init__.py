a84d033704f0631f4b645b06a59dd875ff88f829
threnjen <threnjen@gmail.com>
2024-11-27 07:49:14 -0800

    add all assets to job

--- aws_dagster_bgg/jobs/__init__.py
+++ aws_dagster_bgg/jobs/__init__.py
@@ -2,4 +2,6 @@ from dagster import AssetSelection, define_asset_job
 
 bgg_job = define_asset_job("bgg_job", AssetSelection.all())
 
-user_job = define_asset_job("user_job", selection="scraped_user_xmls")
+user_job = define_asset_job(
+    "user_job", selection=["user_scraper_urls", "scraped_user_xmls"]
+)
