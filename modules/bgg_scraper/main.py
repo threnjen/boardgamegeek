8bc574d079086ddd57df0a88401d3a9c1564c03a
threnjen <threnjen@gmail.com>
2024-11-28 09:35:12 -0800

    move combine files out of scraper

--- modules/bgg_scraper/main.py
+++ modules/bgg_scraper/main.py
@@ -1,18 +1,21 @@
 import argparse
 import os
+import time
 import xml.etree.ElementTree as ET
+from datetime import datetime
+from functools import partial
 
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy_settings import *
-from modules.bgg_scraper.spiders import GameSpider, UserSpider
 
 from config import CONFIGS
+from utils.local_file_handler import LocalFileHandler
 from utils.processing_functions import (
-    get_local_keys_based_on_env,
     load_file_local_first,
     save_file_local_first,
 )
+from utils.s3_file_handler import S3FileHandler
 
 S3_SCRAPER_BUCKET = os.environ.get("S3_SCRAPER_BUCKET")
 IS_LOCAL = True if os.environ.get("IS_LOCAL", "False").lower() == "true" else False
@@ -20,6 +23,134 @@ ENVIRONMENT = os.environ.get("ENVIRONMENT", "dev")
 WORKING_DIR = (
     CONFIGS["dev_directory"] if ENVIRONMENT == "dev" else CONFIGS["prod_directory"]
 )
+
+
+class GameSpider(scrapy.Spider):
+    """Spider to scrape BGG for game data"""
+
+    def __init__(
+        self,
+        name: str,
+        scraper_urls_raw: list[str],
+        save_file_path: str,
+        group: str,
+    ):
+        """
+        Parameters:
+        name: str
+            The name of the spider
+        save_folder: str
+            The folder to save the data to
+        scraper_urls_raw: list[str]
+            The urls to scrape
+        """
+        self.name = name
+        super().__init__()
+        self.scraper_urls_raw = scraper_urls_raw
+        self.save_file_path = save_file_path
+        self.group = group
+
+    def start_requests(self):
+        for i, url in enumerate(self.scraper_urls_raw):
+            print(f"Starting URL {i}: {url}")
+            save_response_with_index = partial(self._save_response, response_id=i)
+            yield scrapy.Request(url=url, callback=save_response_with_index)
+
+    def _save_response(self, response: scrapy.http.Response, response_id: int):
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+        save_file_local_first(
+            path=self.save_file_path,
+            file_name=f"{self.group}_{response_id}_{timestamp}.xml",
+            data=response.body,
+        )
+
+
+class UserSpider(scrapy.Spider):
+    """Spider to scrape BGG for game data"""
+
+    def __init__(
+        self,
+        name: str,
+        scraper_urls_raw: list[str],
+        save_file_path: str,
+        group: str,
+    ):
+        """
+        Parameters:
+        name: str
+            The name of the spider
+        save_folder: str
+            The folder to save the data to
+        scraper_urls_raw: list[str]
+            The urls to scrape
+        """
+        self.name = name
+        super().__init__()
+        self.scraper_urls_raw = scraper_urls_raw
+        self.save_file_path = save_file_path
+        self.group = group
+        self.s3_file_handler = S3FileHandler()
+        self.local_file_handler = LocalFileHandler()
+
+    def start_requests(self):
+        for self.group_num, url in enumerate(self.scraper_urls_raw):
+            user_id = url.split("username=")[-1].split("&rated")[0]
+
+            # check S3 for existing user data
+            if self.s3_file_handler.check_file_exists(
+                file_path=f"{WORKING_DIR}{self.save_file_path}/user_{user_id}.xml"
+            ):
+                # self.download_from_s3()
+                self.logger.info(f"User {user_id} already exists. Skipping...")
+                continue
+
+            self.logger.info(f"Starting URL {self.group_num}: {url}")
+            yield scrapy.Request(
+                url=url,
+                meta={"group_num": self.group_num},
+                dont_filter=True,
+                callback=self.parse,
+            )
+
+    def parse(self, response):
+        if response.status == 429:
+            self.logger.info(
+                "Received 429 status code. Waiting significant time before retry..."
+            )
+            time.sleep(45)
+            yield scrapy.Request(
+                url=response.url,
+                callback=self.parse,
+                dont_filter=True,
+                meta=response.meta,  # Preserve meta information
+            )
+        if (
+            "Your request for this collection has been accepted"
+            in response.body.decode("utf-8")
+        ):
+            self.logger.info("Received 'Request accepted' message. Retrying...")
+            time.sleep(5)
+            yield scrapy.Request(
+                url=response.url,
+                callback=self.parse,
+                dont_filter=True,
+                meta=response.meta,  # Preserve meta information
+            )
+            return
+
+        # Process the valid response
+        self._save_response(response, response.url)
+
+    def _save_response(self, response: scrapy.http.Response, url: str):
+        user_id = url.split("username=")[-1].split("&rated")[0]
+        file_path = f"{WORKING_DIR}{self.save_file_path}/user_{user_id}.xml"
+        save_file_local_first(
+            path=self.save_file_path,
+            file_name=f"user_{user_id}.xml",
+            data=response.body,
+        )
+        self.logger.info(f"Response saved to {file_path}")
 
 
 class DataScraper:
@@ -38,17 +169,6 @@ class DataScraper:
         )
         print(f"Length of incoming urls: {len(scraper_urls_raw)}")
         self._run_scrapy_scraper(scraper_urls_raw)
-        raw_xml = self._combine_xml_files_to_master()
-
-        file_name = CONFIGS[self.scraper_type]["output_raw_xml_suffix"].replace(
-            "{}", self.file_group
-        )
-
-        save_file_local_first(
-            path=self.scraped_files_folder,
-            file_name=file_name,
-            data=raw_xml,
-        )
 
     def _run_scrapy_scraper(self, scraper_urls_raw) -> None:
 
@@ -108,59 +228,6 @@ class DataScraper:
 
         # Testing section to see what happens after we are done with process.start()
         print("Completed with scraping process")
-
-    def _combine_xml_files_to_master(self) -> str:
-        """Combine the XML files into a single XML file"""
-
-        print(f"\n\nCombining XML files for {self.file_group}")
-
-        print(get_local_keys_based_on_env(self.scraped_files_folder))
-
-        saved_files = [
-            x
-            for x in get_local_keys_based_on_env(self.scraped_files_folder)
-            if "combined" not in x and ".gitkeep" not in x
-        ]
-
-        # Parse the first XML file to get the root and header
-        tree = ET.parse(saved_files[0])
-        root = tree.getroot()
-
-        # Create a new root element for the combined XML
-        combined_root = ET.Element(root.tag, root.attrib)
-
-        # Iterate over each XML file
-        for xml_file in saved_files:
-            # Parse the XML file
-            tree = ET.parse(xml_file)
-            root = tree.getroot()
-
-            if self.scraper_type == "users":
-                user_name = xml_file.split("user_")[-1].split(".xml")[0]
-                user_tag = ET.SubElement(combined_root, "username", value=user_name)
-
-                # Append each <item> element to the new root
-                for item in root.findall("item"):
-                    user_tag.append(item)
-            else:
-                # Append each <item> element to the new root
-                for item in root.findall("item"):
-                    combined_root.append(item)
-
-        # Create a new XML tree and write it to a new file
-        combined_tree = ET.ElementTree(combined_root)
-
-        # Get the root element from the ElementTree
-        combined_root = combined_tree.getroot()
-
-        xml_bytes = ET.tostring(combined_root, encoding="utf-8", xml_declaration=True)
-
-        if IS_LOCAL and ENVIRONMENT == "dev":
-            # remove the saved files
-            for xml_file in saved_files:
-                os.remove(xml_file)
-
-        return xml_bytes
 
 
 def parse_args():
