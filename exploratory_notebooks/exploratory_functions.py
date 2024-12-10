6953b57cdb5cd129824082ed6acbcb2c624dd655
threnjen <threnjen@gmail.com>
2024-12-10 09:27:30 -0800

    updates to review gen notebook

--- exploratory_notebooks/exploratory_functions.py
+++ exploratory_notebooks/exploratory_functions.py
@@ -8,11 +8,133 @@ import gc
 import json
 from datetime import datetime
 from typing import Tuple
-import ruptures as rpt
+
+import weaviate
+import weaviate.classes as wvc
+from weaviate.util import generate_uuid5
+from weaviate.classes.query import Filter
+from weaviate.classes.config import Configure
+from sentence_transformers import SentenceTransformer
+
+# import ruptures as rpt
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 # # visualization packages
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+def filter_stopwords(text: str) -> str:
+    stop_words = set(stopwords.words("english"))
+    word_tokens = word_tokenize(text)
+    filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+    return " ".join(filtered_sentence)
+
+
+def evaluate_quality_words_over_thresh(text: str) -> str:
+    word_tokens = word_tokenize(text)
+    return len(word_tokens) > 5
+
+
+def create_weaviate_client() -> weaviate.client:
+    client = weaviate.connect_to_local(
+        headers={
+            "X-HuggingFace-Api-Key": os.environ["HUGGINGFACE_APIKEY"],
+            "X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"],
+        }
+    )
+    return client
+
+
+def add_collection_batch(
+    client: weaviate.client, collection_name: str, game_id: str, reviews: list[str]
+) -> None:
+    collection = client.collections.get(collection_name)
+    print(f"Adding reviews for game {game_id}")
+
+    with collection.batch.dynamic() as batch:
+        for review in reviews:
+            review_item = {
+                "review_text": review,
+                "product_id": game_id,
+            }
+            uuid = generate_uuid5(review_item)
+
+            if collection.data.exists(uuid):
+                continue
+                # if it already exists, update the properties
+                collection.data.update(properties=review_item, uuid=uuid)
+            else:
+                batch.add_object(properties=review_item, uuid=uuid)
+
+
+def generate_aggregated_review(
+    client: weaviate.client, collection_name: str, game_id: str, generate_prompt: str
+) -> str:
+    print(f"Generating aggregated review for game {game_id}")
+    collection = client.collections.get(collection_name)
+    summary = collection.generate.near_text(
+        query="aggregate_review",
+        return_properties=["review_text", "product_id"],
+        filters=Filter.by_property("product_id").equal(game_id),
+        grouped_task=generate_prompt,
+    )
+    return summary
+
+
+def refine_df_for_specific_game(
+    df: pd.DataFrame, game_id: str, sample_pct: float = 0.1
+) -> pd.DataFrame:
+
+    # immediately filter to only the game_id we're interested in
+    df = df[df["BGGId"] == game_id]
+    df = df.reset_index(drop=True)
+    game_name = df["Name"].iloc[0]
+
+    print(f"\n\nBuilding review data frame for game {game_name}: {game_id}")
+
+    # get the ratings sample distribution by taking 10% of the total ratings
+    df["rounded_rating"] = df["rating"].round(0).astype(int)
+    sample_size = int(len(df) * sample_pct)  # Desired total sample size
+    group_sizes = round(
+        df["rounded_rating"].value_counts(normalize=True) * sample_size, 0
+    ).astype(int)
+    print(f"Desired sample size: {sample_size}")
+
+    # refine to only ratings with comments and clean all comments
+    df = df[df["value"].notna()]
+    count_reviews_all_comments = len(df)
+    print(f"Total reviews with comments: {count_reviews_all_comments}")
+    df["value"] = df["value"].replace(r"[^A-Za-z0-9 ]+", "", regex=True)
+    df["value"] = df["value"].str.lower().apply(lambda x: filter_stopwords(x))
+
+    df["quality_review"] = df["value"].apply(evaluate_quality_words_over_thresh)
+    df = df[df["quality_review"] == True]
+    removed_reviews = count_reviews_all_comments - len(df)
+    print(
+        f"Total quality reviews: {len(df)}. {removed_reviews} reviews removed due to quality threshold"
+    )
+
+    if len(df) < sample_size:
+        print("Not enough quality reviews to sample from; using all reviews")
+    else:
+        print(f"Stratified sampling to {sample_size} reviews")
+        rating_counts = df["rounded_rating"].value_counts()
+        # Ensure we don't sample more than the available values in each group
+        adjusted_group_sizes = group_sizes.clip(upper=rating_counts)
+        df = df.groupby("rounded_rating", group_keys=False).apply(
+            lambda x: x.sample(n=int(adjusted_group_sizes[x.name]), random_state=42)
+        )
+
+    # remove all special characters from combined_review
+    df["combined_review"] = df["rating"].astype("string") + " " + df["value"]
+    df["combined_review"] = df["combined_review"].astype("string")
+
+    avg_rating = round(df["AvgRating"].iloc[0], 1)
+    df = df[["BGGId", "Description", "combined_review"]]
+
+    return df, game_name, avg_rating
 
 
 def get_negative_trolls(users):
