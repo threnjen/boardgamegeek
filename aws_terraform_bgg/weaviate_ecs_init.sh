8aac47d8c714e1b881c493dac54668e35742e48b
threnjen <threnjen@gmail.com>
2024-12-11 10:09:46 -0800

    update weaviate ec2 init

--- aws_terraform_bgg/weaviate_ecs_init.sh
+++ aws_terraform_bgg/weaviate_ecs_init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 sudo yum update -y
+sudo yum install -y git
 sudo yum install -y docker
 sudo service docker start
-sudo yum install -y git
 sudo usermod -aG docker ec2-user
 sudo mkdir -p /usr/local/lib/docker/cli-plugins/
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose 
+sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+# sudo yum install -y amazon-ssm-agent
+# sudo systemctl enable amazon-ssm-agent
+# sudo systemctl start amazon-ssm-agent
+# sudo systemctl status amazon-ssm-agent
+
+
