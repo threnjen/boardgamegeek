f76bb58a1587c78c4ce0190ff449e3d6e99afb6a
threnjen <threnjen@gmail.com>
2024-12-12 10:36:33 -0800

    update fargate ecs role

--- aws_terraform_bgg/fargate_ecs_roles.tf
+++ aws_terraform_bgg/fargate_ecs_roles.tf
@@ -23,7 +23,7 @@ module "rag_description_generation_FargateTaskRole_role" {
 
 resource "aws_iam_role_policy_attachment" "rag_description_generation" {
   role       = module.rag_description_generation_FargateTaskRole_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMServiceRolePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "S3_Access_rag_description_generation_FargateTaskRole_attach" {
