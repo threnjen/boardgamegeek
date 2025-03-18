8fcdab7d70c70ba9b003d01d81f017c9878130f0
threnjen <threnjen@gmail.com>
2025-03-17 19:22:46 -0700

    update weaviate init

--- aws_terraform_bgg/weaviate_ecs_init.sh
+++ aws_terraform_bgg/weaviate_ecs_init.sh
@@ -1,24 +1,31 @@
-!/bin/bash
-yum update -y
-yum install -y git
-yum install -y docker
-service docker start
+#!/bin/bash
+set -ex
+
+# Update system packages
+dnf update -y
+
+# Install Docker
+dnf install -y docker
+
+# Start and enable Docker service
+systemctl start docker
+systemctl enable docker
+
+# Add ec2-user to Docker group
 usermod -aG docker ec2-user
+
+# Install Git
+dnf install -y git
+
+# Install Docker Compose (Built-in Plugin)
 mkdir -p /usr/local/lib/docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
 chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-# !/bin/bash
-# sudo yum update -y
-# sudo yum install -y git
-# sudo yum install -y docker
-# sudo service docker start
-# sudo usermod -aG docker ec2-user
-# sudo mkdir -p /usr/local/lib/docker/cli-plugins/
-# sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-# sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-# sudo yum install -y amazon-ssm-agent
-# sudo systemctl enable amazon-ssm-agent
-# sudo systemctl start amazon-ssm-agent
-# sudo systemctl status amazon-ssm-agent
+
+# Ensure permissions take effect
+newgrp docker <<EOF
+docker --version
+EOF
+
 
 
