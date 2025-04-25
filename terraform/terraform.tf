c6e37bdc56948414c70a91657aec80b3ec5cc232
threnjen <threnjen@gmail.com>
2025-04-25 11:16:18 -0700

    refinements and updates to terraform configs

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -23,6 +23,4 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-output "caller_identity" {
-  value = data.aws_caller_identity.current
-}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,11 +7,6 @@ variable "RESOURCE_ENV" {
   description = "Environment for the deployment, e.g., dev, prod"
   type        = string
 }
-variable "BUCKET" {
-  description = "The name of the S3 bucket to store the Terraform state file"
-  type        = string
-}
-
 variable "REGION" {
   description = "AWS region. Must be in string format like: us-west-2"
   type        = string
