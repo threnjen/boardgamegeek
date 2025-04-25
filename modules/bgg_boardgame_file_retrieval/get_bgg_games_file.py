729228c2985c8c90f11020a0d9c0a4276468d444
threnjen <threnjen@gmail.com>
2025-04-25 16:30:14 -0700

    fix minor but important typo

--- modules/bgg_boardgame_file_retrieval/get_bgg_games_file.py
+++ modules/bgg_boardgame_file_retrieval/get_bgg_games_file.py
@@ -14,7 +14,7 @@ from config import CONFIGS
 
 BGG_USERNAME = os.environ.get("BGG_USERNAME")
 BGG_PASSWORD = os.environ.get("BGG_PASSWORD")
-ENVIRONMENT = os.environ.get("TF_VAR_RESOURCE_ENV" "dev")
+ENVIRONMENT = os.environ.get("TF_VAR_RESOURCE_ENV", "dev")
 S3_SCRAPER_BUCKET = CONFIGS["s3_scraper_bucket"]
 IS_LOCAL = True if os.environ.get("IS_LOCAL", "False").lower() == "true" else False
 WORKING_DIR = f"data/{ENVIRONMENT}" if not IS_LOCAL else "/tmp"
